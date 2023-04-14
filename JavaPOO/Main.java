0433289ad572dce03d728d08ce748e2698ac762d
mbodji <ibumbodji@gmail.com>
2023-04-14 10:00:15 +0200

    test de mes methodes dans la classe principale

--- JavaPOO/Main.java
+++ JavaPOO/Main.java
@@ -1,9 +1,49 @@
 package JavaPOO;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+   
+
+        public static void main(String[] args) {
+            /* 
+            ExerciceNote exe =  new ExerciceNote();
+
+            if (args.length == 0) {
+                System.out.println("Veuillez renseigner le chemin du fichier en argument.");
+                return;
+            }
+            
+    
+            String chemin = args[0];
+            System.out.println("voici le premier argument :"+chemin);
+            int nbMots = ExerciceNote.nombreDeMotsDansUnFichier(chemin);
+            System.out.println("Le fichier contient " + nbMots + " mots.");
+    
+            String tab = "merci";
+          
+    
+            System.out.println(exe .reverseString(tab));
+            exe.afficherPyramide();
+            exe.sommeNombre();
+            exe.nomrePair(exe);
+            exe.nombreDeMotsDansUnePhrase();
+            
+            System.out.println("veuiller entrer un nombre pour commencer le jeux ");
+           
+            int saisi = -1;
+            Scanner  sc = new Scanner(System.in);
+            if (sc.hasNextInt()) {
+                saisi = sc.nextInt();
+                exe.jeuxDuJusteNombre(saisi);
+            }else{
+                System.out.println("Veuiller saisir un entier valide");
+            }
+         
+    
+        }
+        */
     }
     
 }
