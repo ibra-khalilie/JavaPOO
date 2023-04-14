e97df08561f5579df0dde4b78bc1ac8fba0c7d13
mbodji <ibumbodji@gmail.com>
2023-04-14 15:00:01 +0200

    initialisation et ajout des entités

---
+++ PartieSpring/JavaExercice/src/test/java/com/example/javaexercice/JavaExerciceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaexercice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExerciceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
